bd295012b01cfbce9bea7c26db554d6b4904408d
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-08-17 19:52:44 -0300

    Feat.: Criando e Estilizando(CSS) Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import Home from "./pages/home/Home"
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Home />
   )
 }
 
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,16 @@
+import './Home.css'
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img 
+                className="img"
+                src="https://i.imgur.com/H88yIo2.png" 
+                alt="Imagem Tela Inicial"
+            />
+        </>
+    )
+}
+
+export default Home
