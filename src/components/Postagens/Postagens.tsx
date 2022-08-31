76559c6bb0726ce4b2aa5f833ddfab4e354a18e2
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-08-31 14:56:17 -0300

    Feat.: Componetizando as Postagens com Props

--- src/components/Postagens/Postagens.tsx
+++ src/components/Postagens/Postagens.tsx
@@ -2,39 +2,43 @@ import { Box, Button, Card, CardActions, CardContent, Typography } from "@mui/ma
 import { Link } from "react-router-dom"
 import Postagem from "../../models/Post"
 
-interface PostagensProps{
+interface PostagensProps {
     post: Postagem
 }
 
 function Postagens({ post }: PostagensProps) {
     return (
-        <Box m={2} >
+        <Box m={2}>
             <Card variant="outlined">
                 <CardContent>
                     <Typography color="textSecondary" gutterBottom>
                         Postagens
                     </Typography>
+
                     <Typography variant="h5" component="h2">
-                        Título
+                        {post.titulo}
                     </Typography>
+
                     <Typography variant="body2" component="p">
-                        Texto da Postagem
+                        {post.texto}
                     </Typography>
+
                     <Typography variant="body2" component="p">
-                        Tema
+                        {post.tema?.descricao}
                     </Typography>
+
                 </CardContent>
                 <CardActions>
                     <Box display="flex" justifyContent="center" mb={1.5}>
 
-                        <Link to="" className="text-decorator-none" >
+                        <Link to={``} className="text-decorator-none" >
                             <Box mx={1}>
                                 <Button variant="contained" className="marginLeft" size='small' color="primary" >
                                     Atualizar
                                 </Button>
                             </Box>
                         </Link>
-                        <Link to="" className="text-decorator-none">
+                        <Link to={``} className="text-decorator-none">
                             <Box mx={1}>
                                 <Button variant="contained" size='small' color="secondary">
                                     Deletar
--- src/pages/ListaPostagem/ListaPostagem.tsx
+++ src/pages/ListaPostagem/ListaPostagem.tsx
@@ -1,8 +1,43 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import useLocalStorage from "react-use-localstorage"
 import Postagens from "../../components/Postagens/Postagens"
+import Postagem from "../../models/Post"
+import { buscar } from "../../services/Services"
 
 function ListaPostagem() {
+    const [posts, setPosts] = useState<Postagem[]>([])
+    const [token, setToken] = useLocalStorage('token')
+
+    let history = useNavigate()
+
+    async function getPost() {
+        await buscar("/postagens", setPosts, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    useEffect(() => {
+        if (token === '') {
+            alert('Você precisa estar logado')
+            history('/')
+        }
+    }, [token])
+
+    useEffect(() => {
+        getPost()
+    }, [posts.length])
+
     return (
-        <Postagens />
+        <>
+            {
+                posts.map(post => (
+                    <Postagens key={post.id} post={post} />
+                ))
+            }
+        </>
     )
 }
 
